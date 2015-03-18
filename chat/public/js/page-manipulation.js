0b0581640f756ae7b2e8e1a555391501fbffb13a
Danny Gibson <dgibson8@lion.lmu.edu>
2015-03-17 23:45:33 -0700

    clicking the settings icon works

--- chat/public/js/page-manipulation.js
+++ chat/public/js/page-manipulation.js
@@ -4,12 +4,18 @@ var options = {
 	show: 'true'
 };
 
+var settingsModal = $('#settings-modal');
+
 $(document).ready(function(){	
-	var settingsModal = $('#settings-modal');
 
 	settingsModal.modal(options);
 	
 	settingsModal.on('shown.bs.modal', function () {
 		$('').focus();
 	});
+});
+
+$('#settings').click( function(){
+  console.log('clicked settings');
+  settingsModal.modal('toggle');
 });
