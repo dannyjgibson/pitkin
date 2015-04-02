eff29b729928ef014aaaebffea017d76f563b9a9
Danny Gibson <dgibson8@lion.lmu.edu>
2015-04-01 20:00:29 -0700

    going to store chatroom data in db

--- chat/index.js
+++ chat/index.js
@@ -1,9 +1,14 @@
-
 var express = require('express'),
     app = express(),
     http = require('http').Server(app),
     path = require('path'),
+    config = require('./config'),
+    superagent = require('superagent'),
     io = require('socket.io')(http);
+
+// read/write chatroom data to db
+mongoose.connect(config.database.test);
+console.log('connected to mongodb at ' + config.database.test);
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
@@ -28,6 +33,13 @@ var setupNamespace = function () {
       namespace.on('connection', function (socket) {
         console.log('someone connected to ' + namespaceId);
       });
+  superagent.post('http://localhost:3000/api/chatrooms')
+    .send({
+      namespaceId: namespace
+    })
+    .end(function(res) {
+      // chatroom added to db. Maybe store mongodb id? 
+    });
 };
 
 var generateNamespaceId = function () {
