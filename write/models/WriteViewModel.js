4bc888f0c51e1c5576f4ae95efc08d4a86631142
Danny Gibson <dgibson8@lion.lmu.edu>
2015-04-18 16:45:08 -0700

    set up view model and can pass user id to server. Next step is plugging this into express to actually get a user for response

--- write/models/WriteViewModel.js
+++ write/models/WriteViewModel.js
@@ -1,28 +1,51 @@
 // model should be set with a get to the db
-var config = require('../config');
+//var config = require('../config');
+
+// This is a crappy shim, and requireJS is the way out
+var configDatabaseTest = 'http://localhost:3000/';
 
 // not every attribute on the model gets added to the Article View Model
 function WriteViewModel (model) {
   var self = this;
-  self.topic = kb.observable();
-  self.title = kb.observable();
-  self.text = kb.observable();
-  self.tags = kb.observableArray();
-  self.writerName = ko.observable();
+  self.topic = ko.observable();
+  self.title = ko.observable();
+  self.text = ko.observable();
+  self.tags = ko.observableArray();
+  self.authorName = ko.observable();
   self.files = ko.observableArray();
 
-  self.getWriteInformation = function (userId) {
+  self.getUserInformation = function (userId) {
+    var userInfoUrl = configDatabaseTest + '/api/users/' + userId;
+    $.getJSON(userInfoUrl, function (data) {
+      self.authorName(userInfo.username);
+      self.files(userInfo.Articles);
+    });
+  };
+
+  self.postWriteInformation = function (updated) {
 
   };
 
-  self.postWriteInformation = function(updated) {
-
+  self.getArticleInformation = function (articleId) {
+    var articleInfoUrl = configDatabaseTest + '/api/articles/' + articleId;
+    $.getJSON(articleInfoUrl, function (data) {
+      self.topic(data.topic);
+      self.title(data.title);
+      self.text(data.text);
+      self.tags(data.tags);
+    });
   };
 
-  self.addTags = function (data, event){
-    if (event.keycode === 13) {
+  self.addTags = function (data, event) {
+    var keyCode = (event.which ? event.which : event.keycode);
+    if (keyCode === 13) {
       var toAdd = data.split(/,?\s+/);
       ko.tags(Array.prototype.push.apply(self.tags(), toAdd));        
     }
+    return true;
   };
+
+  self.getUserInformation(self.authorName());
 }
+
+ko.applyBindings(new WriteViewModel());
