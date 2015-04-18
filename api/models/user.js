9660a708c7082d1b6d46aa3687d1d91cfb07cc83
Danny Gibson <dgibson8@lion.lmu.edu>
2015-04-18 13:39:47 -0700

    tans to spaces, ugh embarassing

--- api/models/user.js
+++ api/models/user.js
@@ -1,34 +1,34 @@
 var mongoose = require('mongoose'),
-	validator = require('validator'),
-	passportLocalMongoose = require('passport-local-mongoose');
+  validator = require('validator'),
+  passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = new mongoose.Schema({
-	emailAddress: String,
-	articleCount: Number,
-	articles: 
-						[
-							{type: mongoose.Schema.Types.ObjectId, ref:'Article'},
-						],
-	createdAt: Date,
-	updatedAt: Date
+  emailAddress: String,
+  articleCount: Number,
+  articles: 
+            [
+              {type: mongoose.Schema.Types.ObjectId, ref:'Article'},
+            ],
+  createdAt: Date,
+  updatedAt: Date
 });
 
 userSchema.plugin(passportLocalMongoose);
 
 //abstract this out
 userSchema.pre('save', function (next) {
-	var currentDate = new Date(),
-			user = this;
-	user.updatedAt = currentDate;
+  var currentDate = new Date(),
+      user = this;
+  user.updatedAt = currentDate;
 
-	if (!this.createdAt) {
-		user.createdAt = currentDate;
-	}
-	// validate properties on save
-	if (!validator.isEmail(user.emailAddress)) {
-		// reject property
-	}
-	next();
+  if (!this.createdAt) {
+    user.createdAt = currentDate;
+  }
+  // validate properties on save
+  if (!validator.isEmail(user.emailAddress)) {
+    // reject property
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
