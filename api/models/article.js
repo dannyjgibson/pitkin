06aa1027cd6132dcadf2c484a1b3f78db8e8eab9
Danny Gibson <dgibson8@lion.lmu.edu>
2015-04-18 16:43:39 -0700

    spacing

--- api/models/article.js
+++ api/models/article.js
@@ -1,26 +1,26 @@
 var mongoose = require('mongoose');
 
 var articleSchema = new mongoose.Schema({
-    topic: String,
-    title: String,
-    author: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-    location: String,
-    publishDate: Date,
-    createdAt: Date,
-    updatedAt: Date,
-    text: String,
-    actions: String,
-    tags: [String]
+  topic: String,
+  title: String,
+  author: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+  location: String,
+  publishDate: Date,
+  createdAt: Date,
+  updatedAt: Date,
+  text: String,
+  actions: String,
+  tags: [String]
 });
 
 articleSchema.pre('save', function (next) {
-	var currentDate = new Date();
-	this.updatedAt = currentDate;
+  var currentDate = new Date();
+  this.updatedAt = currentDate;
 
-	if (!this.createdAt) {
-		this.createdAt = currentDate;
-	} 
-	next();
+  if (!this.createdAt) {
+    this.createdAt = currentDate;
+  } 
+  next();
 });
 
 module.exports = mongoose.model('Article', articleSchema);
--- api/models/user.js
+++ api/models/user.js
@@ -1,60 +1,60 @@
 var mongoose = require('mongoose'),
-	validator = require('validator'),
-	bcrypt = require('bcrypt-nodejs');
+  validator = require('validator'),
+  bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-	emailAddress: String,
-	articleCount: Number,
-	articles: 
-						[
-							{type: mongoose.Schema.Types.ObjectId, ref:'Article'},
-						],
-	createdAt: Date,
-	updatedAt: Date,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  emailAddress: String,
+  articleCount: Number,
+  articles: 
+            [
+              {type: mongoose.Schema.Types.ObjectId, ref:'Article'},
+            ],
+  createdAt: Date,
+  updatedAt: Date,
 });
 
 //abstract this out
 userSchema.pre('save', function (next) {
-	var currentDate = new Date(),
-			user = this;
-	user.updatedAt = currentDate;
+  var currentDate = new Date(),
+      user = this;
+  user.updatedAt = currentDate;
 
-	if (!this.createdAt) {
-		user.createdAt = currentDate;
-	}
+  if (!this.createdAt) {
+    user.createdAt = currentDate;
+  }
 
-	// hash password here
-	if (!user.isModified('password')) {
-		return next();
-	}	
-	user.password = hashPassword(user.password);
-	// validate properties on save
-	if (!validator.isEmail(user.emailAddress)) {
-		// reject property
-	}
-	next();
+  // hash password here
+  if (!user.isModified('password')) {
+    return next();
+  } 
+  user.password = hashPassword(user.password);
+  // validate properties on save
+  if (!validator.isEmail(user.emailAddress)) {
+    // reject property
+  }
+  next();
 });
 
 var hashPassword = function (rawPassword) {
-	return bcrypt.hashSync(rawPassword);
+  return bcrypt.hashSync(rawPassword);
 };
 
 var comparePassword = function (candidate, hashed) {
-	return bcrypt.compareSync(candidate, hashed);
+  return bcrypt.compareSync(candidate, hashed);
 };
 
 var changePassword = function (accountId, newPassword) {
-	var newHashedPassword = hashPassword(newPassword);
-	userSchema.update({_id:accountId}, {$set: {password: newHashedPassword}}, {upsert: false});
-	console.log('change password for ' + accountId);
+  var newHashedPassword = hashPassword(newPassword);
+  userSchema.update({_id:accountId}, {$set: {password: newHashedPassword}}, {upsert: false});
+  console.log('change password for ' + accountId);
 };
 
 var login = function (email, password, next) {
-	userSchema.findOne({email: email, password: hashPassword(password)}, function(err, doc) {
-		next(doc !== null);
-	});
+  userSchema.findOne({email: email, password: hashPassword(password)}, function(err, doc) {
+    next(doc !== null);
+  });
 };
 
 userSchema.statics.comparePassword = comparePassword;
