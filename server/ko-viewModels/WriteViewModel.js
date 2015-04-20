e308dc8cd2dcd1829d884322e1ff1b9ebab2300d
Danny Gibson <dgibson8@lion.lmu.edu>
2015-04-20 10:18:39 -0700

    posting and putting article methods

--- server/ko-viewModels/WriteViewModel.js
+++ server/ko-viewModels/WriteViewModel.js
@@ -15,6 +15,7 @@ function WriteViewModel (model) {
   self.tags = ko.observableArray();
   self.authorName = ko.observable();
   self.files = ko.observableArray();
+  self.articleIdSet = ko.observable(); // maybe it should just check titles?
 
   self.getUserInformation = function (userId) {
     var userInfoUrl = configDatabaseTest + 'api/users/' + userId;
@@ -23,16 +24,54 @@ function WriteViewModel (model) {
       self.userId(userId);
       self.authorName(data.username);
       self.files(data.Articles);
+      var articleIdsFromData = {};
+      for (var file in self.files()) {
+        articleIdsFromData[file.id] = true;
+      }
+      self.articleIdSet(articleIdsFromData);
     });
   };
 
-  self.postWriteInformation = function (updated) {
+  // wrapper method to check if an article already exists
+  self.saveArticleData = function (data, event) {
+    if (data in articleIdSet) {
+      putArticleInformation(data);
+    } else {
+      postArticleInformation(data);
+    }
+  };
 
+  // saving a new article
+  self.postArticleInformation = function (articleInfo) {
+    var articlePOSTUrl = configDatabaseTest + 'api/articles/';
+    $.post(articlePOSTUrl, articleInfo, function (res) {
+      if (res.message.indexOf('success') === -1) {
+        console.log('failed to post ' + articleInfo.id);
+      } else {
+        console.log('post of: ' + articleInfo.id + ' is a success');
+      }
+    });
+  };
+
+  // saving an article that already exists
+  self.putArticleInformation = function (articleInfo) {
+    var articlePUTUrl = configDatabaseTest + 'api/articles/' + articleInfo.id;
+    $.ajax({
+      type: 'PUT',
+      url: articlePUTUrl,
+      contentType: 'application/json',
+      data: JSON.stringify(articleInfo)
+    }).done(function () {
+      console.log('successful PUT to ' + articlePUTUrl);
+    }).fail(function () {
+      console.log('failed to PUT to ' + articlePUTUrl);
+    });
   };
 
   self.getArticleInformation = function (articleId) {
-    var articleInfoUrl = configDatabaseTest + '/api/articles/' + articleId;
+    var articleInfoUrl = configDatabaseTest + 'api/articles/' + articleId;
     $.getJSON(articleInfoUrl, function (data) {
+      self.articleId(data.id);
       self.topic(data.topic);
       self.title(data.title);
       self.text(data.text);
@@ -54,8 +93,6 @@ function WriteViewModel (model) {
   };
 
   var userIdFromDOM = $('.hidden-user-id').text();
-  console.log('userIdFromDOM VVVV');
-  console.log(userIdFromDOM);
   self.getUserInformation(userIdFromDOM);
 }
 
