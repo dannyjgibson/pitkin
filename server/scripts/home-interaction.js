902aef0081e026d98d1bec4b3b3c97f8b50a4d9b
Danny Gibson <dgibson8@lion.lmu.edu>
2015-05-03 00:17:30 -0700

    correct search clearing

--- server/scripts/home-interaction.js
+++ server/scripts/home-interaction.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
   $('#search-by-tags').click(function (argument) {
+    $('.article').not('.dontDisplay').show();
     var searchTags = $('#search-tags').val().split(' ');
-    $('.article').each(function (index) {
-      var articleTags = [];
-      $(this).find('.tag').each(function (index) {
-        articleTags.push($(this).text());
-      });
-      var searchTagPresent = false;
-      for (var i = 0; i < searchTags.length; i++) {
-        if (articleTags.indexOf(searchTags[i]) !== -1){
-          searchTagPresent = true;
+    if (searchTags.indexOf('') === -1) {
+      $('.article').each(function (index) {
+        var articleTags = [];
+        $(this).find('.tag').each(function (index) {
+          articleTags.push($(this).text());
+        });
+        var searchTagPresent = false;
+        for (var i = 0; i < searchTags.length; i++) {
+          if (articleTags.indexOf(searchTags[i]) !== -1){
+            searchTagPresent = true;
+          }
         }
-      }
-      if (!searchTagPresent) {
-        $(this).hide();
-      }
-    });
+        if (!searchTagPresent) {
+          $(this).hide();
+        }
+      });
+    } else {
+      $('.article').not('.dontDisplay').show();
+    }
   });
 });
